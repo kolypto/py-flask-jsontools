9b1bb0d25008b0f626d526de1cd9a34dd5dcdfd8
Dmitry Vasilyanov <vdmit11@gmail.com>
2016-04-01 20:15:44 +0300

    increment package version according to previous changes
    Recently the repository was forked to make some changes in the
    JsonSerializableBase (see previous commit). But the version was not
    incremented.
    
    That adds a bit of confusion, when you have two packages with identical
    versions but different code. So that commit simply fixes that.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 setup(
     # http://pythonhosted.org/setuptools/setuptools.html
     name='flask_jsontools',
-    version='0.1.1-0',
+    version='0.1.2-0',
     author='Mark Vartanyan',
     author_email='kolypto@gmail.com',
 
