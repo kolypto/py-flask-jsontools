2d5a52fd1bcaeaa5d12ab1c933963aa1a3aa93e5
kirk <ekirk0@gmail.com>
2017-03-02 11:54:48 -0800

    deprecation and requirements for test

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=[
         'flask >= 0.10.1',
     ],
+    tests_require=['sqlalchemy',],
     extras_require={},
     include_package_data=True,
     test_suite='nose.collector',
